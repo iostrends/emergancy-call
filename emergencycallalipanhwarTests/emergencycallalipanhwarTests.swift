e65aa21b39408e7694978513d4edca76cfad89be
iostrends <afteryou725@outlook.com>
2018-05-21 12:11:59 +0500

    Initial Commit

---
+++ emergencycallalipanhwarTests/emergencycallalipanhwarTests.swift
@@ -0,0 +1,36 @@
+//
+//  emergencycallalipanhwarTests.swift
+//  emergencycallalipanhwarTests
+//
+//  Created by Mohammad ali panhwar on 5/21/18.
+//  Copyright © 2018 Mohammad ali panhwar. All rights reserved.
+//
+
+import XCTest
+@testable import emergencycallalipanhwar
+
+class emergencycallalipanhwarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
