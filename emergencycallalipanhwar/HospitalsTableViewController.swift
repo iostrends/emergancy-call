35ba8193ecc52271e94fc2b19ce91cf742e74202
iostrends <afteryou725@outlook.com>
2018-07-23 16:00:12 +0500

    Update

---
+++ emergencycallalipanhwar/HospitalsTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HospitalsTableViewController.swift
+//  emergencycallalipanhwar
+//
+//  Created by Mohammad Ali Panhwar on 7/19/18.
+//  Copyright © 2018 Mohammad ali panhwar. All rights reserved.
+//
+
+import UIKit
+
+class HospitalsTableViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+
+}
